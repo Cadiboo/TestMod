f1085287a965f5c4bc14c12d6f6df8ef020e57fb
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2019-02-03 13:43:46 +1100

    Revert "cool stuff"
    This reverts commit 509946a5bc5bbb25a3efb2cd1d8b8b2227f337e4.

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -1,19 +1,14 @@
 package com.example.examplemod;
 
-import com.example.examplemod.client.ClientDistProxy;
-import com.example.examplemod.server.ServerDistProxy;
-import com.example.examplemod.util.IDistProxy;
 import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLDedicatedServerSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
@@ -25,79 +20,64 @@ import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod("examplemod")
-public class ExampleMod {
+public class ExampleMod
+{
+    // Directly reference a log4j logger.
+    private static final Logger LOGGER = LogManager.getLogger();
 
-	// Directly reference a log4j logger.
-	private static final Logger LOGGER = LogManager.getLogger();
+    public ExampleMod() {
+        // Register the setup method for modloading
+        FMLModLoadingContext.get().getModEventBus().addListener(this::setup);
+        // Register the enqueueIMC method for modloading
+        FMLModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
+        // Register the processIMC method for modloading
+        FMLModLoadingContext.get().getModEventBus().addListener(this::processIMC);
+        // Register the doClientStuff method for modloading
+        FMLModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
 
-	public ExampleMod() {
-		// Register the setup method for modloading
-		FMLModLoadingContext.get().getModEventBus().addListener(this::setup);
-		// Register the enqueueIMC method for modloading
-		FMLModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
-		// Register the processIMC method for modloading
-		FMLModLoadingContext.get().getModEventBus().addListener(this::processIMC);
-		// Register the doClientStuff method for modloading
-		FMLModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+        // Register ourselves for server, registry and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
 
-		// Register ourselves for server, registry and other game events we are interested in
-		MinecraftForge.EVENT_BUS.register(this);
-	}
+    private void setup(final FMLCommonSetupEvent event)
+    {
+        // some preinit code
+        LOGGER.info("HELLO FROM PREINIT");
+        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
 
-	//OPTIONAL
-	public static IDistProxy distProxy = DistExecutor.runForDist(
-			() -> ClientDistProxy::new,
-			() -> ServerDistProxy::new
-	);
+    private void doClientStuff(final FMLClientSetupEvent event) {
+        // do something that can only be done on the client
+        LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().gameSettings);
+    }
 
-	private void setup(final FMLCommonSetupEvent event) {
-		// some preinit code
-		LOGGER.info("HELLO FROM PREINIT");
-		LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
-	}
+    private void enqueueIMC(final InterModEnqueueEvent event)
+    {
+        // some example code to dispatch IMC to another mod
+        InterModComms.sendTo("forge", "helloworld", () -> { LOGGER.info("Hello world"); return "Hello world";});
+    }
 
-	private void doClientStuff(final FMLClientSetupEvent event) {
-		// do something that can only be done on the client
-		LOGGER.info("Got client game settings {}", event.getMinecraftSupplier().get().gameSettings);
-	}
+    private void processIMC(final InterModProcessEvent event)
+    {
+        // some example code to receive and process InterModComms from other mods
+        LOGGER.info("Got IMC", event.getIMCStream().
+                map(m->m.getMessageSupplier().get()).
+                collect(Collectors.toList()));
+    }
+    // You can use SubscribeEvent and let the Event Bus discover methods to call
+    @SubscribeEvent
+    public void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
+        // register a new block here
+        LOGGER.info("HELLO from Register Block");
+    }
 
-	private void doServerStuff(final FMLDedicatedServerSetupEvent event) {
-		// do something that can only be done on the server
-		LOGGER.info("Got server profiler {}", event.getServerSupplier().get().profiler);
-	}
-
-	private void enqueueIMC(final InterModEnqueueEvent event) {
-		// some example code to dispatch IMC to another mod
-		InterModComms.sendTo("forge", "helloworld", () -> {
-			LOGGER.info("Hello world");
-			return "Hello world";
-		});
-	}
-
-	private void processIMC(final InterModProcessEvent event) {
-		// some example code to receive and process InterModComms from other mods
-		LOGGER.info("Got IMC", event.getIMCStream().
-				map(m -> m.getMessageSupplier().get()).
-				collect(Collectors.toList()));
-	}
-
-	// You can use SubscribeEvent and let the Event Bus discover methods to call
-	@SubscribeEvent
-	public void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
-		// register a new block here
-		LOGGER.info("HELLO from Register Block");
-	}
-
-	// You can use EventBusSubscriber to automatically subscribe events on the contained class
-	@Mod.EventBusSubscriber
-	public static class ServerEvents {
-
-		@SubscribeEvent
-		public static void onServerStarting(FMLServerStartingEvent event) {
-			// do something when the server starts
-			LOGGER.info("HELLO from server starting");
-		}
-
-	}
-
+    // You can use EventBusSubscriber to automatically subscribe events on the contained class
+    @Mod.EventBusSubscriber
+    public static class ServerEvents {
+        @SubscribeEvent
+        public static void onServerStarting(FMLServerStartingEvent event) {
+            // do something when the server starts
+            LOGGER.info("HELLO from server starting");
+        }
+    }
 }
--- src/main/java/com/example/examplemod/util/IDistProxy.java
+++
@@ -1,8 +0,0 @@
-package com.example.examplemod.util;
-
-/**
- * @author Cadiboo
- */
-public interface IDistProxy {
-
-}
